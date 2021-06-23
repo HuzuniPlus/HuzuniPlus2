f36e895bf1584b0ee982c1a0250bf2989b068165
Warrior80 <68621329+Warrior80@users.noreply.github.com>
2021-06-23 01:49:10 -0300

    Create MixinLayerArmorBase.java

---
+++ src/main/java/me/travis/wurstplus/mixins/MixinLayerArmorBase.java
@@ -0,0 +1,27 @@
+package me.travis.wurstplus.mixins;
+
+import me.travis.wurstplus.Wurstplus;
+import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value={LayerArmorBase.class})
+public class MixinLayerArmorBase {
+    @Inject(method={"doRenderLayer"}, at={@At(value="HEAD")}, cancellable=true)
+    public void doRenderLayer(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale, CallbackInfo ci) {
+        if (Wurstplus.get_hack_manager().get_module_with_tag("NoRender").is_active() && Wurstplus.get_setting_manager().get_setting_with_tag("NoRender", "Armor").get_value(true)) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method={"renderArmorLayer"}, at={@At(value="HEAD")}, cancellable=true)
+    public void renderArmorLayer(EntityLivingBase entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale, EntityEquipmentSlot slotIn, CallbackInfo ci) {
+        if (Wurstplus.get_hack_manager().get_module_with_tag("NoRender").is_active() && Wurstplus.get_setting_manager().get_setting_with_tag("NoRender", "Armor").get_value(true) && slotIn == EntityEquipmentSlot.HEAD) {
+            ci.cancel();
+        }
+    }
+}
