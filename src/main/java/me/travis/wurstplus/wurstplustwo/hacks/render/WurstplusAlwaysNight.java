a779e7e4bc9f5607e311ac5f49b30aa99df80b61
Warrior80 <68621329+Warrior80@users.noreply.github.com>
2021-06-25 01:49:19 -0300

    Time Changer

--- src/main/java/me/travis/wurstplus/wurstplustwo/hacks/render/WurstplusAlwaysNight.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/render/WurstplusAlwaysNight.java
@@ -1,6 +1,7 @@
 package me.travis.wurstplus.wurstplustwo.hacks.render;
 
 import me.travis.wurstplus.wurstplustwo.event.events.WurstplusEventRender;
+import me.travis.wurstplus.wurstplustwo.guiscreen.settings.WurstplusSetting;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusCategory;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusHack;
 import me.zero.alpine.fork.listener.EventHandler;
@@ -11,20 +12,22 @@ public class WurstplusAlwaysNight extends WurstplusHack {
     public WurstplusAlwaysNight() {
         super(WurstplusCategory.WURSTPLUS_RENDER);
 
-        this.name = "Always Night";
-        this.tag = "AlwaysNight";
+        this.name = "Time Changer";
+        this.tag = "TimeChanger";
         this.description = "see even less";
     }
+
+    WurstplusSetting time = create("Time", "TCTime", 18000, 0, 18000);
 
     @EventHandler
     private Listener<WurstplusEventRender> on_render = new Listener<>(event -> {
         if (mc.world == null) return;
-        mc.world.setWorldTime(18000);
+        mc.world.setWorldTime(time.get_value(1));
     });
 
     @Override
     public void update() {
         if (mc.world == null) return;
-        mc.world.setWorldTime(18000);
+        mc.world.setWorldTime(time.get_value(1));
     }
 }
